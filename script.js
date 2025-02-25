c477e5712dd64a4ec0391d3cdf1d5019d6954de1
Albar Malik <albar2806@student.abudzar.sch.id>
2025-02-25 11:52:26 +0700

    bismillah

---
+++ script.js
@@ -0,0 +1,24 @@
+function cprght() {
+    return alert("Copyright © 2025 by Albar Abdul Malik")
+}
+
+function loopBtn() {
+    for (let loopKe = 1; loopKe < 4; loopKe++) {
+        alert(`Tombol 1 diklik, loop ke ${loopKe}`)
+    }   
+}
+function loopBtn2() {
+    for (let loopKe = 1; loopKe < 4; loopKe++) {
+        alert(`Tombol 2 diklik, loop ke ${loopKe}`)
+    }   
+}
+function loopBtn3() {
+    for (let loopKe = 1; loopKe < 4; loopKe++) {
+        alert(`Tombol 3 diklik, loop ke ${loopKe}`)
+    }   
+}
+
+function tambah(a = parseInt(prompt("Masukkan angka pertama")), b = parseInt(prompt("Masukkan angka kedua"))) {
+    c = a + b;
+    return alert(`Hasil dari ${a} + ${b} adalah ${c}`)
+}
